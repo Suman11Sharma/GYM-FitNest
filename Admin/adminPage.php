3a9ee69efa9fb8fe41453cbc130f69c7021699bc
Katrina <katrinabasnetofficial@gmail.com>
2025-11-03 17:19:25 +0545

    working...

--- Admin/adminPage.php
+++ Admin/adminPage.php
@@ -238,6 +238,19 @@ $totalRevenue = $subRevenue + $visitorRevenue + $bookingRevenue;
                             Dashboard
                         </a>
                         <div class="sb-sidenav-menu-heading">Management</div>
+                        <!-- Payments -->
+                        <a class="nav-link collapsed" href="#" data-bs-toggle="collapse"
+                            data-bs-target="#collapsePayments" aria-expanded="false" aria-controls="collapsePayments">
+                            <div class="sb-nav-link-icon"><i class="fas fa-wallet"></i></div>
+                            Payments
+                            <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
+                        </a>
+
+                        <div class="collapse" id="collapsePayments" aria-labelledby="headingPayments" data-bs-parent="#sidenavAccordion">
+                            <nav class="sb-sidenav-menu-nested nav">
+                                <a class="nav-link" href="Payments/payments.php">Payments</a>
+                            </nav>
+                        </div>
 
                         <!-- Paid-Ads  -->
                         <a class="nav-link collapsed" href="#" data-bs-toggle="collapse"
